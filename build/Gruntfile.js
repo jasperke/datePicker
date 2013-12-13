63db990cbcac2f4986ffcbfa87f39d6d84b30a33
Jasper Ke <jasperke@kinghood.com.tw>
2013-12-13 10:40:41 +0800

    use plugin grunt-bump

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -47,12 +47,35 @@ module.exports = function(grunt) {
 					spawn: true,
 				}
 			}
+		},
+
+		// grunt bump 或 grunt bump:patch
+		// grunt bump:minor
+		// grunt bump:major
+		// grunt bump:build ??
+		// grunt bump --setversion=2.0.1
+		// Sometimes you want to run another task between bumping the version and commiting, for instance generate changelog. You can use bump-only and bump-commit to achieve that
+		bump: {
+			options: {
+				files: ['package.json'],
+				updateConfigs: [],
+				commit: true,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'], // '-a' for all files
+				createTag: true,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: false,
+				pushTo: 'master',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bump');
 
 //	grunt.registerTask('dev', ['copy:dev']);
 	grunt.registerTask('default', ['copy:dist', 'uglify']);
